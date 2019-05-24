206fe92ae62eefd101010a1db3a1562d628aa238
Bill Kaguru Wanjohi <bill@ka.guru>
2019-05-24 03:15:38 +0000

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -27,9 +27,8 @@ fn main() -> reqwest::Result<()> {
     const URL: &str = "https://api.coinbase.com/v2/exchange-rates";
     let body: Body = reqwest::get(URL)?.json()?;
     let rates = body.data.rates;
-    println!("JsonValue({})", rates.eth);
-    println!("{:?}", rates.eth);
+    println!("ETC Rate: {}", rates.eth);
     let rate = (Decimal::from(1) / rates.eth).round_dp(2);
-    println!("{:?}", rate);
+    println!("Dollars Per Ether: {}", rate);
     Ok(())
 }
