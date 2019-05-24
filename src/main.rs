4d8dcae942adc2d1ff6aee3e5f6dc5e70ec4bf55
Bill Kaguru Wanjohi <bill@ka.guru>
2019-05-24 03:07:46 +0000

    more specific struct

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,18 @@ extern crate serde;
 
 use rust_decimal::Decimal;
 use serde::Deserialize;
-use std::collections::HashMap;
+
+#[derive(Deserialize)]
+#[serde(rename_all = "UPPERCASE")]
+struct Rates {
+    // btc: Decimal,
+    eth: Decimal,
+}
 
 #[derive(Deserialize)]
 struct Data {
     // currency: String,
-    rates: HashMap<String, Decimal>,
+    rates: Rates,
 }
 
 #[derive(Deserialize)]
@@ -20,11 +26,10 @@ struct Body {
 fn main() -> reqwest::Result<()> {
     const URL: &str = "https://api.coinbase.com/v2/exchange-rates";
     let body: Body = reqwest::get(URL)?.json()?;
-    // println!("{}", json.data.rates["ETH"]);
-    println!("JsonValue({})", body.data.rates["ETH"]);
-    println!("{:?}", body.data.rates["ETH"]);
-    let btc = body.data.rates["ETH"];
-    let rate = (Decimal::from(1) / btc).round_dp(2);
+    let rates = body.data.rates;
+    println!("JsonValue({})", rates.eth);
+    println!("{:?}", rates.eth);
+    let rate = (Decimal::from(1) / rates.eth).round_dp(2);
     println!("{:?}", rate);
     Ok(())
 }
